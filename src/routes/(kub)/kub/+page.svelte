5e912e16f0f693cc4b710de11141d0446ef97974
Maleva Olga <formaleva@gmail.com>
2024-02-02 20:05:53 +0300

    add /kub page

---
+++ src/routes/(kub)/kub/+page.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import "../../../styles.css";
+  import {
+    INIT_DEPENDENCY,
+    KUB_CHECK_STATE_INTERVAL,
+    LEADERS_DATA_LOAD_DELAY
+  } from "$lib/constants";
+  import { invalidate } from "$app/navigation";
+  import { onDestroy } from "svelte";
+  import Logo from "$lib/components/Logo.svelte";
+  import Qr from "$lib/components/QR.svelte";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  async function checkInitData() {
+    if (hasFinished) clearInterval(intervalId);
+    await invalidate(INIT_DEPENDENCY);
+  }
+  let intervalId: number = setInterval(checkInitData, KUB_CHECK_STATE_INTERVAL);
+  $: hasFinished = data && data.serverTime + LEADERS_DATA_LOAD_DELAY > data.startTime;
+
+  onDestroy(() => clearInterval(intervalId));
+</script>
+
+<style>
+  .kub {
+    height: 100vh;
+    width: 100vw;
+    max-height: 720px;
+    max-width: 1280px;
+
+    background-color: var(--color-primary);
+    background-image: var(--kub-bg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .block {
+    width: 520px;
+    height: 520px;
+    border-radius: var(--rounded-xl);
+  }
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: var(--color-primary);
+  }
+  .tagline {
+    margin-top: var(--spacing-extra-large);
+    font-size: var(--font-size-xxl);
+    font-weight: 700;
+  }
+
+  .info {
+    background-color: var(--color-light);
+    padding: 45px;
+    display: flex;
+    align-items: center;
+  }
+  .congrats-text {
+    color: var(--color-dark);
+    font-size: var(--font-size-xxxl);
+  }
+</style>
+
+<div class="kub">
+  <div class="block logo">
+    <Logo width={410} height={150} />
+    <h1 class="tagline">#РусскийТульскийНаш!</h1>
+  </div>
+  <div class="block info">
+    {#if !hasFinished}
+      <Qr />
+    {:else}
+      <h1 class="h1 congrats-text">Поздравляем победителей!</h1>
+    {/if}
+  </div>
+</div>
---
+++ src/routes/(kub)/kub/+page.ts
@@ -0,0 +1,20 @@
+import { error } from "@sveltejs/kit";
+import { getInitData } from "$lib/api";
+import { INIT_DEPENDENCY } from "$lib/constants";
+import type { InitData } from "$lib/types";
+import type { PageLoad } from "./$types";
+
+export const ssr = false;
+
+export const load: PageLoad<InitData | undefined> = async ({ depends }) => {
+  let pageData: InitData | undefined = undefined;
+  depends(INIT_DEPENDENCY);
+
+  try {
+    pageData = await getInitData();
+    if (!pageData) error(500, "Cannot load quiz data");
+  } catch (e) {
+    error(500, "Cannot load quiz data");
+  }
+  return pageData;
+};

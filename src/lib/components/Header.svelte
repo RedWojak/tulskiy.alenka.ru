b83c798c194cb556586b9048d2ddace60afc15e8
Maleva Olga <formaleva@gmail.com>
2024-02-02 10:53:56 +0300

    Add Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Logo from "./Logo.svelte";
+</script>
+
+<style>
+  .header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 21px 42px 0 42px;
+  }
+</style>
+
+<header class="header">
+  <Logo />
+  <h3 class="h3">#РусскийТульскийНаш!</h3>
+</header>

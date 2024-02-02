ae2a6dc4418f2d3f41fa2854b3b4f87ff75c92cc
Maleva Olga <formaleva@gmail.com>
2024-02-02 10:13:25 +0300

    Add Greeting

---
+++ src/lib/components/Greeting.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let userName: string;
+</script>
+
+<style>
+  .wrapper {
+    padding: 0 60px;
+    margin: 10px 0 20px 0;
+  }
+</style>
+
+<div class="wrapper">
+  <h1 class="h2">Добро пожаловать, {userName}!</h1>
+  <h2 class="h3">Игра скоро начнется</h2>
+</div>
